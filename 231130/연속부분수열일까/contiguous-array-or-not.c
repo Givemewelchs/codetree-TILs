41de3645b77923d5a9f09a6691ab43ff44462d22
Givemewelchs <145317688+Givemewelchs@users.noreply.github.com>
2023-11-30 15:27:48 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231130/연속부분수열일까/contiguous-array-or-not.c
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -1,56 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void input(int *p, int k)
-{
+void input(int *p, int k) {
     int i;
-    for (i = 0; i < k; i++)
+    for(i=0;i<k;i++) {
         scanf("%d", &p[i]);
+    }
 }
 
-int fun(int *x, int *y, int n, int m)
-{
-    int i, start;
-    for (i = 0; i < n; i++)
-    {
-        if (x[i] == y[0])
-        {
-            start = i;
-            break;
+int fun(int *x, int *y, int n, int m) {
+    int i, j, start;
+    for(start = 0; start <= n - m; start++) {
+        if(x[start] == y[0]) {
+            for(i = 1; i < m; i++) {
+                if(x[start + i] != y[i]) {
+                    break;
+                }
+            }
+            if(i == m) {
+                return 1;
+            }
         }
     }
-    if (n - start < m)
-        return 0;
-    for (i = start + 1; i < n; i++)
-    {
-        if (x[i] != y[i - start])
-            return 0;
-    }
-    return 1;
+    return 0;
 }
 
-int main()
-{
-    int n, m, sw;
+int main() {
+    int n, m;
     int *p1, *p2;
-
+    int sw;
     scanf("%d %d", &n, &m);
-
-    p1 = (int *)malloc(sizeof(int) * n);
-    p2 = (int *)malloc(sizeof(int) * m);
-
+    p1=(int *)malloc(sizeof(int)*n);
+    p2=(int *)malloc(sizeof(int)*m);
     input(p1, n);
     input(p2, m);
-
     sw = fun(p1, p2, n, m);
-
-    if (sw == 1)
+    if(sw==1) {
         printf("Yes\n");
-    else
+    }
+    else {
         printf("No\n");
-
+    }
     free(p1);
     free(p2);
-
     return 0;
 }
